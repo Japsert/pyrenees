d053de029dcb23266d4eca4631b216805455a038
Jasper Trooster <jasper.trooster@gmail.com>
2026-05-06 10:40:29 +0200

    Update map

--- src/app/map/map.ts
+++ src/app/map/map.ts
@@ -16,14 +16,15 @@ export class Map implements OnInit, OnDestroy {
 
   async ngOnInit() {
     if (isPlatformBrowser(this.platformId)) { // SSR check to ensure this runs in the browser as GL JS requires a browser environment
-      const mapboxgl = (await import('mapbox-gl')).default // dynamically import mapbox-gl as the default export
+      const mapboxgl = (await import('mapbox-gl')).default
 
-      // Create a new map instance
       this.map = new mapboxgl.Map({
         accessToken: environment.MAPBOX_ACCESS_KEY,
-        container: this.mapContainer.nativeElement, // Reference to the map container element
-        center: [-98.54818, 40.00811], // Center coordinates for map over the continental US
-        zoom: 4, // Initial zoom level
+        container: this.mapContainer.nativeElement,
+        style: 'mapbox://styles/japsert-/cmog7wz6t000f01qwgqldfyeo',
+        center: [-0.08138, 42.79418],
+        zoom: 8,
+        pitch: 60,
       });
     }
   }
