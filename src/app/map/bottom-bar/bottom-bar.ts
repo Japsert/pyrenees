e9a685ee4ba1c24dd1886ffff5c90e578d33791e
Jasper Trooster <jasper.trooster@gmail.com>
2026-07-12 11:06:10 +0200

    Update bottom bar debug info

--- src/app/map/bottom-bar/bottom-bar.ts
+++ src/app/map/bottom-bar/bottom-bar.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { StyleSwitcher } from '../style-switcher/style-switcher';
 import { HeightMap } from './height-map/height-map';
 import { StageStats } from './stage-stats/stage-stats';
 import { Flyover } from './flyover/flyover';
+import { InteractionService } from '../../services';
 
 @Component({
   selector: 'app-bottom-bar',
   imports: [StyleSwitcher, StageStats, HeightMap, Flyover],
   templateUrl: './bottom-bar.html',
 })
-export class BottomBar {}
+export class BottomBar {
+  //protected readonly interaction = inject(InteractionService);
+}
